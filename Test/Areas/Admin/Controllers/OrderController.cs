a7741500af539bc103fd8eae158f25c11306e0c8
Nguyễn Thành Đạt <beezune123@gmail.com>
2020-11-04 17:19:52 +0700

    dat commit

---
+++ Test/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.DAL;
+using DataAccess.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Areas.Admin.Controllers
+{
+    public class OrderController : BaseController
+    {
+        // GET: Admin/Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        DataAccess.DAL.Order bll = new DataAccess.DAL.Order();
+        public  List<DataAccess.Entity.Order> Gets()
+        {
+            return bll.GetAll();
+        }
+
+        public bool ApprovedOrder(int id, bool isDenied)
+        {
+            return bll.ApproveOrder(id, isDenied);
+        }
+        public List<OrderDetail> GetDetail(int id)
+        {
+            return bll.GetDetail(id);
+        }
+
+    }
+}
